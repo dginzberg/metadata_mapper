f2e895eee96500530b942146e790de480fc6dfb6
EC2 Default User <ec2-user@ip-10-249-0-28.ap-southeast-1.compute.internal>
2022-08-09 02:45:03 +0000

    env adjustment

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ source .venv/Scripts/activate
 pip install .
 metadata_mapper
 grep -re "^failed" metadata_mapper/logging/*.log --exclude-dir=metadata_mapper/logging/old --exclude=metadata_mapper/logging/failed.log >metadata_mapper/logging/failed.log
-# . /catelas/shared/test.sh
 python -m pytest
+# ENV
+. /catelas/shared/test.sh
